ea2ae3ed50e04f29b55de3908f646106c56e6d1b
Aagya <aagyanagar@gmail.com>
2026-07-12 14:08:03 +0530

    Complete Lesson 01: Programming in Python

---
+++ Part_01_Introduction_to_Python_for_Data_Engineering/Lesson_01_Programming_in_Python/programming_in_python.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+# Arithmetic
+print(23 + 7)
+
+print(40 + 4)
+print(200 - 25 )
+
+print(30 + 10 + 4)
+print(34)
+print(-34)
+
+#Multiplication
+print(16 * 10)
+
+#Division
+print(48 / 5)
+
+# Exponentiation
+print(5 ** 3)
+
