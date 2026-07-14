9c003dc4b959209b89514b459b6ee7371e1273b4
Aagya <aagyanagar@gmail.com>
2026-07-14 17:30:09 +0530

    Complete Lesson 02: Variables and Data Types

---
+++ Part_01_Introduction_to_Python_for_Data_Engineering/Lesson_02_Variables_and_Data_Types/variables_and_datatypes.py
@@ -0,0 +1,38 @@
+a_value = 15
+a_result = (25 - 7) * 17
+print(a_value)
+print(a_result + 12)
+print(a_value + a_result)
+
+income = 34000
+income = income + 6000
+print(income)
+
+variable_1 = 10
+variable_2 = 2.5
+variable_1 += 6.5
+variable_2 *= 2
+print(variable_1)
+print(variable_2)
+
+variable_a = 13.9
+variable_b = 2.8
+variable_a = round(variable_a)
+variable_b = int(variable_b)
+print(variable_a)
+print(variable_b)
+
+app_name = "Pandora - Music & Radio"
+average_rating = "4.0"
+total_ratings = "1724546"
+price = "free"
+print(app_name)
+
+slogan = 'Meta\'s AI slogan is "Explore. Create. Do More."'
+print(slogan)
+
+facebook = "Facebook's rating is"
+fb_rating = 3.5
+fb_rating_str = str(fb_rating)
+fb = facebook + " " + fb_rating_str
+print(fb)
